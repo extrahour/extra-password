a8e76ca1648c6076160b95bea89d253ba89e0e44
Donatas Utmanas <donatas.utmanas@devbridge.com>
2017-11-22 22:39:02 +0200

    Package structure and ignore IntelliJ project files

---
+++ src/main/java/org/extrahour/password/ExtraPassword.java
@@ -0,0 +1,8 @@
+package org.extrahour.password;
+
+public class ExtraPassword {
+
+    public static void main(String[] args) {
+        System.out.println("extrahour.org :: extra-password");
+    }
+}
